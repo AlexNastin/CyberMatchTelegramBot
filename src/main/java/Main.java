44f4eb2a84d0b7c06fdbebda76f54aef5c58e3f8
Alex Nastin <alexnastin1992@gmail.com>
2016-11-21 12:26:21 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+import com.cybermatch.bot.SimpleBot;
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ApiContextInitializer.init();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        try {
+            telegramBotsApi.registerBot(new SimpleBot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
